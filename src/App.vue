64510cde72f972a366b2fb2f0aebb1e548151f02
Enyer045 <enyerber045gmail.com>
2026-02-12 23:52:48 -0400

    add: primer componente

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <script setup>
+import HeaderComponent from './components/HeaderComponent.vue';
+
 
 </script>
 
 <template>
-  <h1>Hola a todos</h1>
+  <HeaderComponent />
+  <h1 class="title">Hola a todos</h1>
 </template>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
 
+.title {
+  color: red;
+  background-color: yellow;
+}
 </style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,22 @@
+<script setup></script>
+
+<template>
+  <header class="title">
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+.title {
+  background-color: #333;
+  color: white;
+  padding: 1rem;
+}
+
+</style>
